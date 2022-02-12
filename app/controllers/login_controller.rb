a2ef9a501a51f433c3ab90b6c67d94b04699fda2
baka <dyatlovleonid@gmail.com>
2022-02-12 22:44:22 +0300

    e

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -11,6 +11,6 @@ class LoginController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.permit(:username, :password)
   end
 end
